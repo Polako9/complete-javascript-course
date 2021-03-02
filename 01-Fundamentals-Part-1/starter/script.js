a4cc1a3f5c4d221e9b8ff4aae9c206ac25cf163e
PolakoM <marcin.polak93@gmail.com>
2021-03-02 21:10:13 +0100

    data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -20,3 +20,34 @@ let continent = 'European continent';
 let population = '`38mln';
 
 console.log(country , continent , population);
+
+/*primitives types
+Number
+String
+Boolean
+Undefined ex let children;
+Null 'eampy value';
+Symbol (ES2015)
+BigInt (ES2020);!!!
+
+CTRL + / TO COMmENT TEXT
+*/
+
+let javascriptIsFun  = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof 'Marcinek');
+
+javascriptIsFun = 'YES!';
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 93;
+console.log(typeof year);
+
+console.log(typeof null);
+
