f075cf852ae624579f4334d136e4ff818b3ed41a
PolakoM <marcin.polak93@gmail.com>
2021-03-02 20:47:56 +0100

    values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,13 @@ let js = 'amazing';
 if (js === 'amazing') alert('Javascript is cool');
 
 console.log(40 + 8);
+
+console.log('Marcinek');
+
+let firstName = 'Marcinek';
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Artist';
+
+console.log(myFirstJob);
